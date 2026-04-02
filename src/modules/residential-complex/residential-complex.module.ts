02a316072e93e76967b80ac2b9f605cbb48fd88e
maicolqj <maicolqj.crow@gmail.com>
2026-04-02 18:05:04 -0500

    feat: register HttpModule and GeocodingService in ResidentialComplexModule

--- src/modules/residential-complex/residential-complex.module.ts
+++ src/modules/residential-complex/residential-complex.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 
 import { ResidentialComplex }           from './entities/residential-complex.entity';
 import { Building }                     from './entities/building.entity';
@@ -8,29 +9,33 @@ import { Unit }                         from './entities/unit.entity';
 import { ResidentialComplexService }    from './services/residential-complex.service';
 import { BuildingService }              from './services/building.service';
 import { UnitService }                  from './services/unit.service';
+import { GeocodingService }             from './services/geocoding.service';
 
 import { ResidentialComplexResolver }   from './resolvers/residential-complex.resolver';
 import { BuildingResolver }             from './resolvers/building.resolver';
 import { UnitResolver }                 from './resolvers/unit.resolver';
-import { User } from '../users/entities/user.entity';
+import { User }        from '../users/entities/user.entity';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ResidentialComplex, Building, Unit, User]),
+    AuditModule,
+    HttpModule.register({
+      timeout: 5000,
+      headers: { 'User-Agent': 'Residash/1.0' },
+    }),
   ],
   providers: [
-    // Servicios
     ResidentialComplexService,
     BuildingService,
     UnitService,
-    // Resolvers
+    GeocodingService,
     ResidentialComplexResolver,
     BuildingResolver,
     UnitResolver,
   ],
   exports: [
-    // Exportamos los servicios para que otros módulos puedan usarlos
-    // (ej: ResidentsModule necesitará UnitService y ResidentialComplexService)
     ResidentialComplexService,
     BuildingService,
     UnitService,
